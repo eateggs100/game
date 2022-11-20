a7dce95a1cea645ba0599b7b273802fedbd536bb
Wiggin Y <viking.yu@189.cn>
2022-11-20 16:23:42 +0800

    production version

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { FC, useMemo } from "react"
 import { AssetProvider } from "./context/Asset"
-//import { Router } from "./router"
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { Dashboard } from "./pages/Dashboard"
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
+import { clusterApiUrl } from '@solana/web3.js';
 import "./App.css"
 
 
 export const App = () => {
-  const endpoint = "https://muddy-aged-panorama.solana-devnet.discover.quiknode.pro/0fe7822c98ade63f96ae1be8e82d17b26d57cacc/"
+  // const endpoint = "https://muddy-aged-panorama.solana-devnet.discover.quiknode.pro/0fe7822c98ade63f96ae1be8e82d17b26d57cacc/"
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
@@ -20,7 +23,6 @@ export const App = () => {
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <AssetProvider>
-          {/* <Router /> */}
           <Dashboard/>
         </AssetProvider>
       </WalletProvider>
